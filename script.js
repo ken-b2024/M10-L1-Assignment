d585bbafeaab7334f28cd7d7aa5edc88f5ff4941
ken-b2024 <kenneth.brown.2404@gmail.com>
2025-03-29 00:00:53 -0400

    M10 L1 Assignment

---
+++ script.js
@@ -0,0 +1,48 @@
+// Exercise 1
+let studentName = "Jason";
+let studentAge = 17;
+let studentGrade = 74;
+let isPresent = true;
+
+console.log("Name:", studentName);
+console.log("Age:", studentAge);
+console.log("Grade:", studentGrade);
+console.log("Present:", isPresent);
+
+// Exercise 2
+let num1 = 5;
+let num2 = 16;
+
+let sum = num1 + num2;
+let diff = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+// Task 3
+console.log("Add:", sum);
+console.log("Subtract:", diff);
+console.log("Multiply:", mult);
+console.log("Divide:", div);
+
+// Task 4
+let updatedNum1 = 25;
+let updatedNum2 = 12;
+
+console.log("Updated Num1:", updatedNum1 + num2);
+console.log("Updated Num2:", num1 + updatedNum2);
+
+// Task 5
+let isEqualTo = num1 == num2;
+let isGreaterThan = num1 > num2;
+let isNotEqualTo = num1 != num2;
+
+console.log("Is Num1 equal to Num2?:", isEqualTo);
+console.log("Is Num1 greater than Num2?:", isGreaterThan);
+console.log("Is Num1 NOT equal to Num2?:", isNotEqualTo);
+
+// Task 6
+let isPositive = num1 % 2 === 0 && num2 % 2 === 0;
+let isOneEven = num1 % 2 === 0 || num2 % 2 === 0;
+
+console.log("Are both numbers positive?:", isPositive);
+console.log("Is at least one number even?:", isOneEven);
